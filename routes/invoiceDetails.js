aadb6e8d7081f57569fd7b9049f5a28cb87ed6b5
jmartinrivera11 <josemartinriverasolis@gmail.com>
2026-03-11 21:36:57 -0600

    agregar endpoint DELETE detalle de factura

--- routes/invoiceDetails.js
+++ routes/invoiceDetails.js
@@ -1,7 +1,9 @@
 var routes = require("express").Router();
 var InvoiceDetailsController = require("../controllers/invoiceDetails");
 let createInvoiceDetail = InvoiceDetailsController.createInvoiceDetail;
+let deleteInvoiceDetail = InvoiceDetailsController.deleteInvoiceDetail;
 
+routes.delete("/delete/:invoiceDetailID", deleteInvoiceDetail);
 routes.post("/create", createInvoiceDetail);
 
 module.exports = routes;
