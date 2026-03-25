85dae00eff681326442c9aacf5fdfa889d008bae
rajuria <rajuria@unitec.edu>
2026-03-24 18:20:14 -0600

    Creado enPoint para solicitar Cancelacion de la orden
    Se creo un endpoint para actualizar el estado de la orden a "Cancelacion Solicitada".

--- routes/orders.js
+++ routes/orders.js
@@ -4,9 +4,11 @@ var ordersController = require("../controllers/orders");
 let createOrder = ordersController.createOrder;
 let modifyOrderStatus = ordersController.modifyOrderStatus;
 let asignarOrden = ordersController.asignarOrden;
+let solicitarCancelacion = ordersController.solicitarCancelacion;
 
 routes.post("/create", createOrder);
 routes.put("/modifyStatus/:orderID", modifyOrderStatus);
 routes.put("/asignarOrden/:orderID/:vendedor", asignarOrden);
+routes.put("/solicitarCancelacion/:orderID", solicitarCancelacion);
 
 module.exports = routes;
